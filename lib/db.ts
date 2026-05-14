387d45be4f94a5edfe45fe45d4062d7d3ccb636d
Agustin Davila <agustindavila637@gmail.com>
2026-05-14 11:21:29 -0300

    still working on prisma

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,7 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/prisma/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  const connectionString = process.env.DATABASE_URL;
-  if (!connectionString) {
-    throw new Error("DATABASE_URL is not set");
-  }
-  const adapter = new PrismaPg({ connectionString });
-  return new PrismaClient({ adapter });
+  return new PrismaClient();
 };
 
 declare global {
